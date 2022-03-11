6bb261703fc553d9a1a99867c727644bfefbb6e5
Control Panel <80440243+MarcAMOO2@users.noreply.github.com>
2022-03-11 11:34:25 +1000

    base files upload
    includes the main page, css, javascript and jquery

---
+++ main.js
@@ -0,0 +1,39 @@
+
+    var x = "Is the browser online? " + navigator.onLine;
+    console.log("Browser online status:  " + navigator.onLine + ", that is good")
+
+
+
+    /* easy page identify lol */
+console.log("");
+console.log("██╗░░██╗░█████╗░███╗░░░███╗███████╗██████╗░░█████╗░░██████╗░███████╗");
+console.log("██║░░██║██╔══██╗████╗░████║██╔════╝██╔══██╗██╔══██╗██╔════╝░██╔════╝");
+console.log("███████║██║░░██║██╔████╔██║█████╗░░██████╔╝███████║██║░░██╗░█████╗░░");
+console.log("██╔══██║██║░░██║██║╚██╔╝██║██╔══╝░░██╔═══╝░██╔══██║██║░░╚██╗██╔══╝░░");
+console.log("██║░░██║╚█████╔╝██║░╚═╝░██║███████╗██║░░░░░██║░░██║╚██████╔╝███████╗");
+console.log("╚═╝░░╚═╝░╚════╝░╚═╝░░░░░╚═╝╚══════╝╚═╝░░░░░╚═╝░░╚═╝░╚═════╝░╚══════╝");
+
+
+
+/* I had this but then it had no purpose.. */
+
+/*
+var btn = document.getElementById("foo");
+var interval;
+
+btn.addEventListener("mouseover", function() {
+  if(!interval)
+    interval = setInterval(switchfunction, 500);
+});
+
+function switchfunction() {
+  if (document.body.style.background === 'red') {
+    document.body.style.background = 'green';
+  } else {
+    document.body.style.background = 'red';
+  }
+}
+
+*/
+
+/* I had this but then it had no purpose.. */
